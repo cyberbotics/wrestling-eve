526ce5f42153722e3c493b489e989a9f931818c7
Léo Duggan <leo.duggan@alumni.epfl.ch>
2023-01-13 09:51:42 +0100

    Updated to new utils

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -17,19 +17,15 @@ Demonstrates how to use the camera and gives an image processing example to loca
 """
 
 from controller import Robot, Motion
+import cv2
 import sys
 sys.path.append('..')
-from utils.fall_detection import FallDetection # David's fall detection is implemented in this class
-from utils.finite_state_machine import FiniteStateMachine
 from utils.current_motion_manager import CurrentMotionManager
+from utils.finite_state_machine import FiniteStateMachine
+from utils.image_processing import ImageProcessing as IP
 from utils.running_average import RunningAverage
-import utils.image
-
-try:
-    import cv2
-except ImportError:
-    sys.exit("Warning: 'cv2' module not found. Please check the Python modules installation instructions " +
-             "at 'https://www.cyberbotics.com/doc/guide/using-python'.")
+from utils.fall_detection import FallDetection  # David's fall detection is implemented in this class
+from utils.camera import Camera
 
 
 class Eve (Robot):
@@ -48,10 +44,7 @@ class Eve (Robot):
             }
         )
 
-        # camera
-        self.camera = self.getDevice("CameraTop")
-        self.camera.enable(self.time_step)
-
+        self.camera = Camera(self)
         self.gps = self.getDevice("gps")
         self.gps.enable(self.time_step)
 
@@ -60,15 +53,13 @@ class Eve (Robot):
         self.LShoulderRoll = self.getDevice("LShoulderRoll")
 
         self.fall_detector = FallDetection(self.time_step, self)
-
         self.current_motion = CurrentMotionManager()
         # load motion files
         self.motions = {
             'SideStepLeft': Motion('../motions/SideStepLeftLoop.motion'),
-            'TurnLeft':     Motion('../motions/TurnLeft20.motion'),
-            'TurnRight':    Motion('../motions/TurnRight20.motion'),
+            'TurnRight': Motion('../motions/TurnRight20.motion'),
+            'TurnLeft': Motion('../motions/TurnLeft20.motion'),
         }
-
         self.opponent_position = RunningAverage(dimensions=1)
 
     def run(self):
@@ -99,17 +90,17 @@ class Eve (Robot):
     def _get_normalized_opponent_horizontal_position(self):
         """Returns the horizontal position of the opponent in the image, normalized to [-1, 1]
             and sends an annotated image to the robot window."""
-        img = utils.image.get_cv_image_from_camera(self.camera)
+        img = self.camera.get_image()
         largest_contour, vertical, horizontal = self.locate_opponent(img)
         output = img.copy()
         if largest_contour is not None:
             cv2.drawContours(output, [largest_contour], 0, (255, 255, 0), 1)
             output = cv2.circle(output, (horizontal, vertical), radius=2,
                                 color=(0, 0, 255), thickness=-1)
-        utils.image.send_image_to_robot_window(self, output)
+        self.camera.send_to_robot_window(output)
         if horizontal is None:
             return 0
-        return horizontal * 2/img.shape[1] - 1
+        return horizontal * 2 / img.shape[1] - 1
 
     def locate_opponent(self, img):
         """Image processing demonstration to locate the opponent robot in an image."""
@@ -124,10 +115,11 @@ class Eve (Robot):
         closing = cv2.morphologyEx(
             thresh, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15)))
         # the robot is assumed to be the largest contour
-        largest_contour = utils.image.get_largest_contour(closing)
+        largest_contour = IP.get_largest_contour(closing)
         if largest_contour is not None:
             # we get its centroid for an approximate opponent location
-            vertical_coordinate, horizontal_coordinate = utils.image.get_contour_centroid(largest_contour)
+            vertical_coordinate, horizontal_coordinate = IP.get_contour_centroid(
+                largest_contour)
             return largest_contour, vertical_coordinate, horizontal_coordinate
         else:
             # if no contour is found, we return None
